3a6eab0900675a0eb1cac422ec0d267f8c0bf171
Jacob Luszcz <jacob.luszcz@gmail.com>
2018-01-19 07:21:33 -0500

    Exclude boto from lambda zip

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ ZIP_NAME="$CWD/burgerlist.zip"
 rm -f "$ZIP_NAME"
 zip -9 "$ZIP_NAME"
 pushd "lib/python2.7/site-packages"
-zip -r9 "$ZIP_NAME" *
+zip -r9 "$ZIP_NAME" $(ls | grep -v boto)
 popd
 zip -g "$ZIP_NAME" burgerlist.py
