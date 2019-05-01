b22115b9b562aca2071420e824f7a7c969dadd68
Jacob Luszcz <jacob.luszcz@gmail.com>
2019-05-01 07:26:28 -0400

    Update aws provider version and apply public access block to generator buckets

--- list-of-lists.tf
+++ list-of-lists.tf
@@ -12,6 +12,7 @@ variable "aws_region" {
 
 provider "aws" {
   region = "${var.aws_region}"
+  version = "~> 2.0"
 }
 
 provider "aws" {
@@ -143,6 +144,15 @@ resource "aws_cloudfront_distribution" "site_distribution" {
 
 resource "aws_s3_bucket" "generator" {
   bucket = "${var.site_url}-generator"
+}
+
+resource "aws_s3_bucket_public_access_block" "generator_public_block" {
+  bucket = "${aws_s3_bucket.generator.id}"
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_object" "index_template" {
